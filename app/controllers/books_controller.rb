f01ecadf02adbfccf88a0989fdf36fcf546e58ca
augustleague <shanyuanshengye896@gmail.com>
2021-12-08 19:17:47 +0000

    second commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
 
   def show
     @book = Book.find(params[:id])
     @user = current_user
+    @book_new = Book.new
   end
 
   def index
@@ -36,9 +39,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
@@ -46,6 +49,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
 end
