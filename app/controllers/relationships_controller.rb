531863f46c76dbac33693b663a9b3323b89af9cc
augustleague <shanyuanshengye896@gmail.com>
2021-12-10 14:40:00 +0000

    ouyou4.2 commit

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,5 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   def create
     current_user.follow(params[:user_id])
     redirect_to request.referer
@@ -8,4 +9,15 @@ class RelationshipsController < ApplicationController
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+  
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,28 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :name, length: {minimum: 2, maximum: 20}, uniqueness: true
+  validates :introduction, length: { maximum: 50}
 
   has_many :books
     has_many :book_comment, dependent: :destroy
     has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :following_user, through: :follower, source: :followed
-  has_many :follower_user, through: :followed, source: :follower
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+
   
   def follow(user_id)
-    follower.create(followed_id: user_id)
+    relationships.create(followed_id: user_id)
   end
   
   def unfollow(user_id)
-    follower.find_by(followed_id: user_id).destroy
+    relationships.find_by(followed_id: user_id).destroy
   end
   
   def following?(user)
-    following_user.include?(user)
+    followings.include?(user)
   end
-
-  validates :name, length: {minimum: 2, maximum: 20}, uniqueness: true
-  validates :introduction, length: { maximum: 50}
 end
