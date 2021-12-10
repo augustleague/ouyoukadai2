82009283b0a9096cbb02dc598247c3d0901b0a84
augustleague <shanyuanshengye896@gmail.com>
2021-12-10 12:27:23 +0000

    ouyou4.1 commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    member do
+      get :follows, :followers
+    end
+    resources :relationships, only: [:create, :destroy]
+  end
   
 end
 
