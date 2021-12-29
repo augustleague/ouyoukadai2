b4ae1ebbce8c8584331d27f84b51db0c1958f20b
augustleague <shanyuanshengye896@gmail.com>
2021-12-29 05:19:43 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root 'homes#top'
   get "home/about" => "homes#about"
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
+  get '/search', to: 'searches#search'
   
 end
 
